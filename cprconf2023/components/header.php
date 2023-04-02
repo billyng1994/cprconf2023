348012e3b0c6c8a0880527fef9e3bee930ad7719
billyng1994 <billyng1994work@gmail.com>
2023-04-02 17:52:42 +0800

    v 4.0.13
    - clean up the code

--- cprconf2023/components/header.php
+++ cprconf2023/components/header.php
@@ -2,49 +2,30 @@
 
     <div class="container" id="banner" style="width:100%">
         <div class="row" style="position: relative;">
-                
-            <!-- <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <div style="display:flex;flex-wrap:wrap;justify-content:space-evenly;">
-                <img src="images/poly.jpg" class="img-responsive logo" alt="PolyU">
-                <img src="images/speed.jpg" class="img-responsive logo" alt="speed">
+            <div class="container " style="display:flex;width:100%;justify-content:center;flex-wrap:wrap;">
+                <div class="col" style="display:flex">
+                    <img src="images/poly.jpg" class="img-responsive logo" alt="PolyU">
+                    <img src="images/speed.jpg" class="img-responsive logo" alt="speed">
+                </div>
+
+                <div class="col" style="display:flex">
                 <img src="images/HSUHK_Logo.jpg" class="img-responsive logo" alt="HSUHK">
                 <img src="images/THEi_Logo.png" class="img-responsive logo" alt="THEi">
+                </div>
+                <div class="col" style="display:flex">
                 <img src="images/SZU_Logo.jpeg" class="img-responsive logo" alt="SZU">
                 <img src="images/IFTM_CI_Logo.jpg" class="img-responsive logo" alt="IFTM_CI">
+                </div>
+
+                <div class="col" style="display:flex">
                 <img src="images/UIC-logo.jpg" class="img-responsive logo" alt="UIC">
                 <img src="images/CCAPPTIA_Logo.jpeg" class="img-responsive logo" alt="CCAPPTIA">
-                <img src="images/IOS_Logo.jpg" class="img-responsive logo" alt="IOS">
-                <img src="images/HKSTLA_Logo.png" class="img-responsive logo" alt="HKSTLA">
                 </div>
-            </div> -->
-
-                <div class="container " style="display:flex;width:100%;justify-content:center;flex-wrap:wrap;">
-                    <div class="col" style="display:flex">
-                        <img src="images/poly.jpg" class="img-responsive logo" alt="PolyU">
-                        <img src="images/speed.jpg" class="img-responsive logo" alt="speed">
-                    </div>
-
-                    <div class="col" style="display:flex">
-                    <img src="images/HSUHK_Logo.jpg" class="img-responsive logo" alt="HSUHK">
-                    <img src="images/THEi_Logo.png" class="img-responsive logo" alt="THEi">
-                    </div>
-                    <div class="col" style="display:flex">
-                    <img src="images/SZU_Logo.jpeg" class="img-responsive logo" alt="SZU">
-                    <img src="images/IFTM_CI_Logo.jpg" class="img-responsive logo" alt="IFTM_CI">
-                    </div>
-
-                    <div class="col" style="display:flex">
-                    <img src="images/UIC-logo.jpg" class="img-responsive logo" alt="UIC">
-                    <img src="images/CCAPPTIA_Logo.jpeg" class="img-responsive logo" alt="CCAPPTIA">
-                    </div>
-                    <div class="col" style="display:flex">
+                <div class="col" style="display:flex">
                     <img src="images/IOS_Logo.jpg" class="img-responsive logo" alt="IOS">
                     <img src="images/HKSTLA_Logo.png" class="img-responsive logo" alt="HKSTLA">
-                    </div>
                 </div>
-                
-
-
+            </div>
         </div>
     </div>
     <!-- <div class="container" id="banner">
